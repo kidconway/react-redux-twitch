5e85c4e83e6fce384135d6a9a3b593c45e892700
Colby Horan <colbyhoran@gmail.com>
2020-09-03 11:04:08 -0500

    refactored google auth

--- client/src/components/streams/CreateStream.js
+++ client/src/components/streams/CreateStream.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CreateStream = () => {
-  return <div>CreateStream</div>;
+  return (
+    <div>
+      <h1 className="header">Create a Stream</h1>
+    </div>
+  );
 };
 
 export default CreateStream;
